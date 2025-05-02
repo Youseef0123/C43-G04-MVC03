4150790b368109e0658f02ea56d7e1e473b88e52
youssef mahmoud <99423758+Youseef0123@users.noreply.github.com>
2025-05-02 22:29:44 +0300

    Session MVC 08

---
+++ Demo.presntation/Utilities/EmailSetting.cs
@@ -0,0 +1,25 @@
+﻿using System.Net;
+using System.Net.Mail;
+using System.Security.Policy;
+
+namespace Demo.presntation.Utilities
+{
+    public class EmailSetting
+    {
+        public static void SendEmail(Email email)
+        {
+            //Sending Email
+            try
+            {
+                var Client = new SmtpClient("smtp.gmail.com", 587);
+                Client.EnableSsl = true;
+                Client.Credentials = new NetworkCredential("youoseefmemo2016@gmail.com", "wmogkpevwfjdtokx");
+                Client.Send("youoseefmemo2016@gmail.com", email.To, email.Subject, email.Body);
+
+            }catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
---
+++ Demo.presntation/ViewModels/ResetPasswordViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Demo.presntation.ViewModels
+{
+    public class ResetPasswordViewModel
+    {
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage ="Password Is Required")]
+        public string PassWord { get; set; }
+
+
+        [DataType(DataType.Password)]
+        [Compare(nameof(PassWord))]
+        public string ConfrimPassword { get; set; }
+
+    }
+}
